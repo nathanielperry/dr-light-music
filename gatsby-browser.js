7f48e28c887367a7b644c6c1718c5bc583503f76
Nathaniel Perry <nathaniel.r.perry@gmail.com>
2021-07-01 21:52:03 -0500

    Switching back to wrapPageElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,6 @@ import React from 'react';
 import './src/styles/global.css';
 import Layout from './src/components/Layout';
 
-export function wrapRootElement({ element, props }) {
+export function wrapPageElement({ element, props }) {
     return <Layout {...props}>{element}</Layout>
 }
